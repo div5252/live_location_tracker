9813cd1ff3857c7e8e4b3555a5197aa0c345ff47
TenzonUltra <52448449+TenzonUltra@users.noreply.github.com>
2021-07-14 23:19:43 +0530

    Updated App logo

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       
+    debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.orange,
       ),
